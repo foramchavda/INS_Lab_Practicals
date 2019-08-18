2cc8481dc2ab7d35a09085827318f74c8c796ac8
foramchavda <46369066+foramchavda@users.noreply.github.com>
2019-08-18 23:55:57 +0530

    Update Columnar.java

--- Columnar.java
+++ Columnar.java
@@ -1,64 +1,139 @@
 import java.util.*;
+import java.io.*;
+import java.lang.*;
 
-class Columnar
-{
-	public static void main(String a[])
-	{
-		int l=a[0].length()/a[1].length();
-		if(a[0].length()%a[1].length()!=0)
-			l++;
-		char[][] tmp=new char[l][a[1].length()];
-		char[] strArr=a[0].toCharArray();
-		int i=0;
-		System.out.println("Enter 1 for encryption\nEnter 2 for decryption");
-		Scanner s=new Scanner(System.in);
-		int c=s.nextInt();
-		int d,m,n;
-		StringBuffer ans=new StringBuffer();
-		if(c==1)
-		{
-			for(m=0;m<l;m++)
-			{
-				for(n=0;n<a[1].length();n++)
-				{
-					if(i<strArr.length)
-						tmp[m][n]=strArr[i++];
-					else
-						tmp[m][n]='-';
-				}
-			}
-			for(i=1;i<=a[1].length();i++)
-			{
-				d=a[1].indexOf((char)(i+'0'));
-				for(int j=0;j<l;j++)
-				{
-					ans.append(Character.toString(tmp[j][d]));
-				}
-			}
-			System.out.println("Encrypted Text: "+ans);
+public class columnarTranspose {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String line = System.getProperty("line.separator");
+        scan.useDelimiter(line);
+
+        System.out.print("1. Encryt 2.Decrypt : ");
+        int option = scan.nextInt();
+        switch (option) {
+            case 1:
+                System.out.print("Enter String:");
+                String text = scan.next();
+
+                System.out.print("Enter Key:");
+                String key = scan.next();
+
+                System.out.println(encryptCT(key, text).toUpperCase());
+                break;
+            case 2:
+                System.out.print("Enter Encrypted String:");
+                text = scan.next();
+
+                System.out.print("Enter Key:");
+                key = scan.next();
+
+                System.out.println(decryptCT(key, text));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static String encryptCT(String key, String text) {
+        int[] arrange = arrangeKey(key);
+
+		for(int i=0; i<arrange.length; i++) {
+			System.out.println("Key: " + arrange[i]);
 		}
-		else
-		{
-			m=0;
-			for(i=1;i<=a[1].length();i++)
-			{
-				d=a[1].indexOf((char)(i+'0'));
-				for(int j=0;j<l;j++)
-				{
-					tmp[j][d]=strArr[m++];
-				}
-			}
-			for(n=0;n<l;n++)
-			{
-				for(m=0;m<a[1].length();m++)
-				{
-					if(tmp[n][m]=='-')
-						break;
-					else
-						ans.append(tmp[n][m]);
-				}
-			}
-			System.out.println("Decrypted Text: "+ans);
+
+        int lenkey = arrange.length;
+        int lentext = text.length();
+
+        int row = (int) Math.ceil((double) lentext / lenkey);
+
+        char[][] grid = new char[row][lenkey];
+        int z = 0;
+        for (int x = 0; x < row; x++) {
+            for (int y = 0; y < lenkey; y++) {
+                if (lentext == z) {
+                    // at random alpha for trailing null grid
+                    grid[x][y] = RandomAlpha();
+                    z--;
+                } else {
+                    grid[x][y] = text.charAt(z);
+                }
+
+                z++;
+            }
+        }
+        String enc = "";
+        for (int x = 0; x < lenkey; x++) {
+            for (int y = 0; y < lenkey; y++) {
+                if (x == arrange[y]) {
+                    for (int a = 0; a < row; a++) {
+                        enc = enc + grid[a][y];
+                    }
+                }
+            }
+        }
+        return enc;
+    }
+
+    public static String decryptCT(String key, String text) {
+        int[] arrange = arrangeKey(key);
+        int lenkey = arrange.length;
+        int lentext = text.length();
+
+        int row = (int) Math.ceil((double) lentext / lenkey);
+
+        String regex = "(?<=\\G.{" + row + "})";
+        String[] get = text.split(regex);
+		for(int i=0; i<get.length; i++) {
+			System.out.println("get: " + get[i]);
 		}
-	}
+
+        char[][] grid = new char[row][lenkey];
+
+        for (int x = 0; x < lenkey; x++) {
+            for (int y = 0; y < lenkey; y++) {
+                if (arrange[x] == y) {
+                    for (int z = 0; z < row; z++) {
+                        grid[z][y] = get[arrange[y]].charAt(z);
+                    }
+                }
+            }
+        }
+
+        String dec = "";
+        for (int x = 0; x < row; x++) {
+            for (int y = 0; y < lenkey; y++) {
+                dec = dec + grid[x][y];
+            }
+        }
+
+        return dec;
+    }
+
+    public static char RandomAlpha() {
+        //generate random alpha for null space
+        Random r = new Random();
+        return (char)(r.nextInt(26) + 'a');
+    }
+
+    public static int[] arrangeKey(String key) {
+        //arrange position of grid
+        String[] keys = key.split("");
+		for(int i=0; i<key.length(); i++)
+		{
+			System.out.println("Split: " + keys[i]);
+		}
+        Arrays.sort(keys);
+        int[] num = new int[key.length()];
+        for (int x = 0; x < keys.length; x++) {
+            for (int y = 0; y < key.length(); y++) {
+                if (keys[x].equals(key.charAt(y) + "")) {
+                    num[y] = x;
+                    break;
+                }
+            }
+        };
+		//System.out.println("ArrangeKey/Num: " + num[0]);
+        return num;
+    }
+
 }
